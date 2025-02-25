19bac6dfcb89b91fdbbc1e1564a7eb612d899419
Alexander Waya Nguku <alex.waya@gmail.com>
2025-02-26 02:10:32 +0300

    Setup the user model

--- users/models.py
+++ users/models.py
@@ -1,9 +1,16 @@
-from django.db import models
-
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext as _
 
+from .managers import CustomUserManager
 
+class CustomUser(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username',)
 
-class User(AbstractUser):
-    pass
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
